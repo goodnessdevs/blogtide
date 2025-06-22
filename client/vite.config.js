a569de9c5c3e063bbdea393a0d84b5b19d3d25a7
isaacjosh71@gmail.com <isaacjosh71@gmail.com>
2025-06-22 21:45:38 +0100

    Last commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,23 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // increase limit from default 500kb
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+
   define: {
     global: "window",
   },

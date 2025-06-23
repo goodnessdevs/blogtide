a416d3c233d5a1195b392b13b696f1df56a42b8b
isaacjosh71@gmail.com <isaacjosh71@gmail.com>
2025-06-23 04:58:18 +0100

    Updated database file

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -5,7 +5,6 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    // await mongoose.connection.collection("users").dropIndex("username_1");
     console.log(`MongoDB connected: ${conn.connection.host} ✅`);
   } catch (error) {
     console.log("Error connection to MONGODB: ", "❌", error);
